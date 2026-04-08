944dee2955441ef06d2006f87b571333f2c2825d
Nicholas Malart <support@ghghomesafety.com.au>
2026-04-09 08:02:27 +1000

    Changes to be committed:

--- tradinginformation/xabbo-main/xabbo-main/src/Xabbo/Services/Abstractions/IUiContext.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Xabbo.Services.Abstractions;
-
-public interface IUiContext
-{
-    bool IsSynchronized { get; }
-    void Invoke(Action callback);
-    Task InvokeAsync(Action callback);
-}
